e145ae499f2a7b1ad8e103f4851967298cbca8ae
LongJinwen <204084802@qq.com>
2022-06-07 11:43:34 +0800

    chore: 更新依赖

--- src/view/nav.ts
+++ src/view/nav.ts
@@ -5,7 +5,6 @@ export default [
   { name: 'template', link: '/template', C: React.lazy(() => import('@/view/template')) },
   { name: 'table', link: '/table', C: React.lazy(() => import('@/view/table')) },
   { name: 'virtual', link: '/virtual', C: React.lazy(() => import('@/view/virtual')) },
-  { name: 'md', link: '/md', C: React.lazy(() => import('@/view/md')) },
   { name: 'env', link: '/env', C: React.lazy(() => import('@/view/env')) },
   { name: 'useRequest', link: '/useRequest', C: React.lazy(() => import('@/view/useRequest')) },
   { name: 'await-to-js', link: '/await-to-js', C: React.lazy(() => import('@/view/await-to-js')) },
