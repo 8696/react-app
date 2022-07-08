40daf9b0a2c95073fd9500208803cfe582c313cf
LongJinwen <204084802@qq.com>
2022-07-08 13:56:37 +0800

    feat: git:commit

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -43,7 +43,7 @@ const babelRuntimeEntryHelpers = require.resolve(
 const babelRuntimeRegenerator = require.resolve('@babel/runtime/regenerator', {
   paths: [babelRuntimeEntry]
 })
-
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 // Some apps do not need the benefits of saving a web request, so not inlining the chunk
 // makes for a smoother build process.
 const shouldInlineRuntimeChunk = process.env.INLINE_RUNTIME_CHUNK !== 'false'
@@ -592,6 +592,7 @@ module.exports = (webpackEnv) => {
       ].filter(Boolean)
     },
     plugins: [
+      process.env.ANALYZE === 'true' && new BundleAnalyzerPlugin(),
       // Generates an `index.html` file with the <script> injected.
       new HtmlWebpackPlugin(
         Object.assign(
