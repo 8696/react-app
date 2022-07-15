5387b2d78a3966bb85d7f64fd0ad85b5b03bdd63
LongJinwen <204084802@qq.com>
2022-07-15 15:29:49 +0800

    chore: 降级react17

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
+import ReactDOM from 'react-dom'
 import React from 'react'
 
-import { createRoot } from 'react-dom/client'
-
 import reportWebVitals from './reportWebVitals'
-
 import './index.less'
-
-const root = createRoot(document.getElementById('root') as HTMLElement)
 
 const App = React.lazy(() => import('./App'))
 
-root.render(<React.Suspense fallback={<></>}><App /></React.Suspense>)
+ReactDOM.render(<React.Suspense fallback='loading...'><App /></React.Suspense>, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
