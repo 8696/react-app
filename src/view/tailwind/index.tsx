981674d450e3531f85b89ec3bc45881693e4b695
LongJinwen <204084802@qq.com>
2022-09-23 16:04:54 +0800

    chore: 更新TailwindCSS配置

--- src/view/tailwind/index.tsx
+++ src/view/tailwind/index.tsx
@@ -1,6 +1,5 @@
 import './index.less'
 import style from './index.module.less'
-import Container from './component/container'
 import BoxSizing from './component/box-sizing'
 import Display from './component/display'
 import Theme from './component/theme'
@@ -8,8 +7,6 @@ import Theme from './component/theme'
 export default () => {
   return (
     <div style={{ padding: 20 }}>
-      <div className={`tailwind-title ${style.tailwindTitleM}`}>Container</div>
-      <Container />
       <div className={`tailwind-title ${style.tailwindTitleM}`}>box sizing</div>
       <BoxSizing />
       <div className={`tailwind-title ${style.tailwindTitleM}`}>display</div>
