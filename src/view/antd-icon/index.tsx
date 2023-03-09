09e6352c930315a1044a1bea4dce78bf35188882
LongJinwen <204084802@qq.com>
2023-03-09 20:10:14 +0800

    feat: 调整样式

--- src/view/antd-icon/index.tsx
+++ src/view/antd-icon/index.tsx
@@ -2,7 +2,8 @@ import './index.less'
 import { StarOutlined, StarFilled, StarTwoTone } from '@ant-design/icons'
 export default () => {
   return (
-    <div style={{ padding: 20 }}>
+    <div className='m-view'>
+      <h3 className='m-title'>Ant design icons</h3>
       <StarOutlined />
       <StarFilled />
       <StarTwoTone twoToneColor='#eb2f96' />
