93e291be2ef41b990765654122dd36ca32560429
LongJinwen <204084802@qq.com>
2024-03-13 14:32:08 +0800

    feat: 调整env

--- src/view/env/index.tsx
+++ src/view/env/index.tsx
@@ -1,6 +1,7 @@
 import './index.less'
+import envConfig from '@/env.config'
 export default () => {
-  const json = JSON.stringify(process.env, null, '  ')
+  const json = JSON.stringify(envConfig, null, '  ')
   return (
     <div style={{ padding: 20 }}>
       <pre>{json}</pre>
