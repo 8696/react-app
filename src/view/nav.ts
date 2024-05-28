7823009cfc4d4751f65a87c5c988a7916fc4b413
LongJinwen <204084802@qq.com>
2024-05-28 15:31:11 +0800

    feat: 升级package.json

--- src/view/nav.ts
+++ src/view/nav.ts
@@ -5,7 +5,6 @@ export default [
   { name: 'antd', link: '/antd', C: React.lazy(() => import('@/view/antd')) },
   { name: 'template', link: '/template', C: React.lazy(() => import('@/view/template')) },
   { name: 'table', link: '/table', C: React.lazy(() => import('@/view/table')) },
-  { name: 'virtual', link: '/virtual', C: React.lazy(() => import('@/view/virtual')) },
   { name: 'env', link: '/env', C: React.lazy(() => import('@/view/env')) },
   { name: 'use-request', link: '/use-request', C: React.lazy(() => import('@/view/use-request')) },
   { name: 'await-to-js', link: '/await-to-js', C: React.lazy(() => import('@/view/await-to-js')) },
