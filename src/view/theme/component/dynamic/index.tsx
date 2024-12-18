2da59beeb8f4156bad3ffc8097cc0410146cce07
LongJinwen <396983001@qq.com>
2024-12-18 19:01:07 +0800

    feat: 新增动态主题demo

--- src/view/theme/component/dynamic/index.tsx
+++ src/view/theme/component/dynamic/index.tsx
@@ -1,6 +1,7 @@
-import { theme, ColorPicker, Button } from 'antd'
+import { theme, ColorPicker, Button, Alert } from 'antd'
 import { useContext, useEffect } from 'react'
 import { ThemeContext } from '@/provider/theme'
+import Form from './form'
 
 const { useToken } = theme
 export default () => {
@@ -13,23 +14,64 @@ export default () => {
 
   const { setTheme } = useContext(ThemeContext)
 
+
   return (
     <>
       <div className='m-view'>
         <h3 className='m-title'>动态设置</h3>
         <div>
           <Button type='primary'>Primary Button</Button>
+          <br/>
+          <br/>
+          <Form/>
         </div>
         <br/>
         <div className='flex items-center'>
-          <span>选择颜色：</span>
+          <span>选择主颜色：</span>
           <ColorPicker onChange={value => {
-            setTheme && setTheme({
+            setTheme?.({
               token: {
+                ...token,
                 colorPrimary: '#' + value.toHex()
               }
             })
-          }} />
+          }}/>
+        </div>
+        <div className='flex items-center'>
+          <span>选择主hover颜色：</span>
+          <ColorPicker onChange={value => {
+            setTheme?.({
+              token: {
+                ...token,
+                colorPrimaryHover: '#' + value.toHex()
+              }
+            })
+          }}/>
+        </div>
+        <div className='flex items-center'>
+          <span>选择error颜色：</span>
+          <ColorPicker onChange={value => {
+            setTheme?.({
+              token: {
+                ...token,
+                colorError: '#' + value.toHex()
+              }
+            })
+          }}/>
+        </div>
+        <br/>
+        <div className='flex items-center'>
+          <Button
+            type='primary'
+            onClick={() => {
+              setTheme?.({
+                token: {
+                  ...token,
+                  borderRadius: 20
+                }
+              })
+            }}
+          >设置圆角</Button>
         </div>
       </div>
     </>
